e67f90745cae0e760c3c70f8647f5e93fe75fe8c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-18 21:20:39 -0300

    feat: add Archetype abstract.

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  constructor(private _name: string, private _special = 0, private _cost = 0) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
