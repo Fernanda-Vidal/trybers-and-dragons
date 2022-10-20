b776d66b9827bf05fd0ee7ad27b61aa09e8daee7
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-19 21:30:38 -0300

    refactor: attribute protected Monster class.

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  constructor(private _lifePoints = 85, private _strength = 63) {}
+  constructor(protected _lifePoints = 85, private _strength = 63) {}
 
   get lifePoints(): number {
     return this._lifePoints;
