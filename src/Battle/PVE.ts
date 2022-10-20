d7ba2c9660542bb29d48b2fbbc2f837c9f8a2e66
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-19 23:47:56 -0300

    chore: coment verification battle PVE.

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -6,20 +6,20 @@ export default class PVE extends Battle {
     super(fighter);
   }
 
-  battleEnds(): boolean {
-    if (this.fighter.lifePoints !== -1
-      && this.environmental
-        .some((char) => char.lifePoints !== -1)) return false;
-    return true;
-  }
+  // battleEnds(): boolean {
+  //   if (this.fighter.lifePoints !== -1
+  //     && this.environmental
+  //       .some((char) => char.lifePoints !== -1)) return false;
+  //   return true;
+  // }
 
   fight(): number {
-    while (!this.battleEnds()) {
-      this.environmental.forEach((char) => {
-        this.fighter.attack(char);
-        char.attack(this.fighter);
-      });
-    }
+    // while (!this.battleEnds()) {
+    this.environmental.forEach((char) => {
+      this.fighter.attack(char);
+      char.attack(this.fighter);
+    });
+    // }
     return super.fight();
   }
 }
